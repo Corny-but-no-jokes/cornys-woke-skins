fc0c6cf58f0d2a2c5cb51b4323e1e6a9a8529a5f
Edgars Lejnieks <elejnieks867@gmail.com>
2023-03-10 19:13:30 +0200

    Minor changes

--- config.cpp
+++ config.cpp
@@ -1,5 +1,5 @@
 class CfgPatches {
-    class {mod class name here} {
+    class {CRNY_WokeSkins} {
         units[] = {};
         weapons[] = {};
         requiredVersion = 0.1;
@@ -7,25 +7,4 @@ class CfgPatches {
     };
 };
 
-class cfgWeapons {
-    class Vest_Base;
-    class VestItem;
-    
-    class example_PlateCarrier1_rgr : Vest_Base {
-        scope = 2;
-        displayName = "Corny's Woke Vest (sketch)";
-        picture = "\A3\characters_f\Data\UI\icon_V_plate_carrier_1_CA.paa";
-        model = "\A3\Weapons_F\DummyVest.p3d";
-        hiddenSelections[] = {"camo"};
-        hiddenSelectionsTextures[] = {"Data\vests_blk_co.paa"};
-        
-        class ItemInfo : VestItem {
-            uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02";
-            containerClass = "Supply100";
-            mass = 50;
-            armor = 5*0.5;
-            passThrough = 0.7;
-            hiddenSelections[] = {"camo"};
-        };
-    };
-};
+#include "CfgWeapons.hpp"

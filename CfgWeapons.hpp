d85d610fadd5ce5b3dfc78d1d86cd1b19ae2f0fb
Edgars Lejnieks <elejnieks867@gmail.com>
2023-03-11 10:48:54 +0200

    edits to classname

--- CfgWeapons.hpp
+++ CfgWeapons.hpp
@@ -2,11 +2,11 @@ class cfgWeapons {
     class Vest_Base;
     class VestItem;
     
-    class CRNY_GoofyAhhVestSketch : Vest_Base {
+    class V_PlateCarrier2_blk : Vest_Base {
 		author = "Corny, but no jokes"
         scope = 0;
         displayName = "Corny's Woke Vest (sketch)";
-        picture = "\A3\characters_f\Data\UI\icon_V_plate_carrier_1_CA.paa";
+        picture = "\A3\characters_f\Data\UI\icon_V_plate_carrier_2_blk_CA.paa";
         hiddenSelections[] = {"camo"};
         hiddenSelectionsTextures[] = {"cornys-woke-skins\Data\vests_blk_co.paa"};
         

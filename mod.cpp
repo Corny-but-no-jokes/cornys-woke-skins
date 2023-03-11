e952f0205ef27b5a89b789e5d19684a5baf99410
Edgars Lejnieks <elejnieks867@gmail.com>
2023-03-11 15:14:03 +0200

    Directory restructure

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "Corny's-Woke-Skins";
+picture = "logo.paa";
+description = "You WOULD NOT want to wear these";
+logo = "logo.paa";
+logoOver = "logo.paa";
+tooltip = "Corny's-Woke-Skin";
+tooltipOwned = "Corny's-Woke-Skin Owned";
+overview = "Corny's-Woke-Skins";
+author = "Corny, but no jokes";
+overviewPicture = "logo.paa";
+overviewText = "Corny's-Woke-Skins";
+overviewFootnote = "";

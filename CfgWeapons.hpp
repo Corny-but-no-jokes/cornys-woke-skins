4291e89446c857f6dadee18ce0657334f984b91c
Edgars Lejnieks <elejnieks867@gmail.com>
2023-03-11 14:20:26 +0200

    redone cfgweps

--- CfgWeapons.hpp
+++ CfgWeapons.hpp
@@ -1,3 +1,4 @@
+/*
 class cfgWeapons {
     class Vest_Base;
     class VestItem;
@@ -26,4 +27,39 @@ class cfgWeapons {
 			};
         };
     };
+};
+*/
+
+class CfgWeapons
+{
+	class ItemCore;
+	class Vest_Camo_Base : ItemCore
+	{
+		class ItemInfo;
+	};
+	class CRNY_PlateCarrier2_cum : Vest_Camo_Base
+	{
+		author = "Corny, but no jokes";
+		scope = 2;
+		displayName = "Sketches";
+		picture = "\A3\characters_f\Data\UI\icon_V_plate_carrier_2_blk_CA.paa";
+		model = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+		hiddenSelectionsTextures[] = {"cornys-woke-skins\Data\vests_blk_co.paa"};
+		class ItemInfo : ItemInfo
+		{
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+			containerClass = Supply100;
+			mass = 50;
+			
+			class HitpointsProtectionInfo //more info at: https://community.bistudio.com/wiki/Arma_3_Soldier_Protection
+			{
+				class Chest
+				{
+						hitPointName = "HitChest";
+						armor = 5*0.7;
+						passThrough = 0.7;
+				};
+			};
+		};
+	};
 };
